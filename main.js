2b4bd022377e01c2edaab657930acd57f9bbde9b
IvanoPisaniello <ivanop92@gmail.com>
2023-07-14 14:11:23 +0200

    created objects array

---
+++ main.js
@@ -0,0 +1,50 @@
+// Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	            scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
+
+
+
+//creo un array di oggetti inserendo i dati all'interno degli oggetti: parolaChiave: "proprietà"
+const team = [
+    {
+        firstName: "Waine",
+        lastName: "Barnett",
+        role: "Founder & Ceo",
+        img: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        firstName: "Angela",
+        lastName: "Caroll",
+        role: "Chief Editor",
+        img: "angela-caroll-chief-editor.jpg",
+    },
+    {
+        firstName: "Walter",
+        lastName: "Gordon",
+        role: "Office Manager",
+        img: "walter-gordon-office-manager.jpg",
+    },
+    {
+        firstName: "Angela",
+        lastName: "Lopez",
+        role: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        firstName: "Scott",
+        lastName: "Estrada",
+        role: "Developer",
+        img: "scott-estrada-developer.jpg",
+    },
+    {
+        firstName: "Barbara",
+        lastName: "Ramos",
+        role: "Grafic Designer",
+        img: "Barbara-ramos-grafic-designer.jpg",
+    }
+]
+
+console.log(team)
